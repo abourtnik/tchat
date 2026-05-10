ea1b4359f7e0e6b2f32c293dfb4dd6340e6a5c79
abourtnik <contact@antonbourtnik.fr>
2026-05-10 21:31:53 +0200

    Test CI

--- resources/js/components/Username.tsx
+++ resources/js/components/Username.tsx
@@ -9,6 +9,8 @@ type Data = {
 }
 export function Username () {
 
+    console.log('Test');
+
     const [edit, setEdit] = useState<boolean>(false);
     const [username, setUsername] = useState<string>(window.USER.username);
 
